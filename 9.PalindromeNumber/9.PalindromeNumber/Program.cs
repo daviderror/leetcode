55dcabb3b2e5d3e2ecbd84ae25e0f0dfd5c45016
David <david.chalyan25@gmail.com>
2022-06-12 00:57:22 +0300

    9.PalindormNumber

---
+++ 9.PalindromeNumber/9.PalindromeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _9.PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            solution.IsPalindrome(141);
+        }
+    }
+    public class Solution
+    {
+        public bool IsPalindrome(int x)
+        {
+            string c = x.ToString();
+            int a = c.Length - 1, b = 0;
+            while (b < a)
+            {
+                if (c[a] != c[b])
+                    return false;
+                b++;
+                a--;
+            }
+            return true;
+        }
+    }
+}
